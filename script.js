61775afc4ad0f13ea652d779ef6802db4a8ae966
SriMoorthy <srimoorthy27@gmail.com>
2024-10-08 13:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let out = document.getElementById('output')
+function Output() {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(out.innerHTML = 'Hello, world!')
+		},1000)
+	})
+}
+
+Output();
